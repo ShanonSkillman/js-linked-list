d4f1514e8cabad95c6cc04ba8238454531dcd6a0
Djavan Cackley <djavancackley@Djavans-MacBook-Pro.local>
2019-01-15 19:12:57 -1000

    almost solved

--- linkedList.js
+++ linkedList.js
@@ -33,17 +33,59 @@ function linkedListGenerator(){
         return tail;
     }
 
-    let get = (number) => {
-        return get;
-    }
+    let get = (num) => {
+        let node = head
+    for(let i =0; i <num; i++){
+        if(node.next === null){
+            return false}
+            node = node.next;
+        }
+        return node;
+    } 
+   
+    
 
-    let remove = (number) => {
-        return remove;
-    }
+    let remove = (num) => {
+        if (num === 0) {
+            head = head.next;
+        } else if (get(num) === tail) {
+            get(num - 1).next = null;
+            tail = get(num - 1);
+        } else if (get(num) === false) {
+            return false
+        } else {
+            get(num - 1).next = get(num + 1);
+        }
+    };
+    
+    //     //    let previous = 
+    // //    let next =   
+    // // console.log(num)
+    // // console.log("head", head)
+    // deleteMe = get(num)
+    // // console.log(deleteMe)
+    // previous = get(num)
+    // console.log(previous)
+    //     return remove;
+    // }
 
-    let insert = (number) => {
-        return insert;
-    }
+    let insert = (value, n) => {
+        let added = {};
+        added.value = value;
+        let before = get(n - 1);
+
+        if (n === 0) {
+            added.next = head;
+            head = added;
+
+        } else if (get(n) === false){
+            return false;
+
+        } else {
+            added.next = get(n);
+            before.next = added;
+        }
+    };
 
     
 
@@ -60,3 +102,5 @@ function linkedListGenerator(){
 
 
 // const insert = () =>{};
+
+       
